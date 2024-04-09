d8d3a796a51190045a9cb6b96b57bef2490641ba
Jimin Yoo <jmy0819_@naver.com>
2024-04-09 15:03:05 +0900

    feat: Buffer size test

--- streamlink/main.py
+++ streamlink/main.py
@@ -1,19 +1,32 @@
-import streamlink, time
+import streamlink, datetime, sys, time
 
-URL = "https://www.youtube.com/watch?v=-rX9VpMLxOk"
-TIME = 5
-BUF_SIZE = 1024
+# URL = sys.argv[1]
+# BUF_SIZE = int(sys.argv[2])
+# TIME = int(sys.argv[3])
+
+URL = "https://www.youtube.com/watch?v=Z2MR73ELLkE"
+BUF_SIZE = 4096
+TIME = 300
 
 streams = streamlink.streams(URL)
 print("Streams found: ", *streams.keys())
 
-fd = streams['480p'].open()
+fd = streams['best'].open()
 
-for i in range(3):
-    print(f"Video #{i+1}...")
-    fname = f"video-{i}.ts"
-    with open(fname, "wb") as video_binary:
-        now = time.time()
+now = time.time()
+lines = 0
+total_data = 0 
 
-        while time.time() - now < TIME:
-            video_binary.write(fd.read(BUF_SIZE))
+with open("vid.ts", "wb") as video_file:
+    while time.time() - now < TIME:
+        data = fd.read(BUF_SIZE)
+        lines += 1
+        total_data += len(data)
+
+        video_file.write(data)
+
+        # TODO: Implement Kafka producer
+        if len(data) < BUF_SIZE:
+            print(f"{datetime.datetime.now()} | {lines} {len(data)}/{BUF_SIZE}")
+
+print(f"Utilization: {total_data / (lines * BUF_SIZE) :.4f}")
