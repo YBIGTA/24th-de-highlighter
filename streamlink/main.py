43b67f1f8efe5bafb6fe74be103b994bf24b3bd4
Jimin Yoo <jmy0819_@naver.com>
2024-05-03 19:26:55 +0900

    feat: Send message to SQS

--- streamlink/main.py
+++ streamlink/main.py
@@ -1,12 +1,13 @@
-import streamlink, datetime, sys, time
+import streamlink, datetime, sys, time, boto3
+import base64
 
-# URL = sys.argv[1]
-# BUF_SIZE = int(sys.argv[2])
-# TIME = int(sys.argv[3])
+URL = sys.argv[1]
+BUF_SIZE = int(sys.argv[2])
+TIME = int(sys.argv[3])
 
-URL = "https://www.youtube.com/watch?v=Z2MR73ELLkE"
-BUF_SIZE = 4096
-TIME = 300
+# URL = "https://www.youtube.com/watch?v=Z2MR73ELLkE"
+# BUF_SIZE = 4096
+# TIME = 300
 
 streams = streamlink.streams(URL)
 print("Streams found: ", *streams.keys())
@@ -15,18 +16,21 @@ fd = streams['best'].open()
 
 now = time.time()
 lines = 0
-total_data = 0 
+total_data = 0
 
-with open("vid.ts", "wb") as video_file:
-    while time.time() - now < TIME:
-        data = fd.read(BUF_SIZE)
-        lines += 1
-        total_data += len(data)
+# AWS SQS
+sqs = boto3.client('sqs')
+queue_url='https://sqs.ap-northeast-2.amazonaws.com/710063216674/highlighter.fifo'
 
-        video_file.write(data)
-
-        # TODO: Implement Kafka producer
-        if len(data) < BUF_SIZE:
-            print(f"{datetime.datetime.now()} | {lines} {len(data)}/{BUF_SIZE}")
-
-print(f"Utilization: {total_data / (lines * BUF_SIZE) :.4f}")
+# with open("vid.ts", "wb") as video_file:
+data_id = 0
+while time.time() - now < TIME:
+    data = fd.read(BUF_SIZE)
+    data_id += 1
+    res = sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=base64.b64encode(data).decode(),
+            MessageGroupId="1",
+            MessageDeduplicationId=str(data_id)
+    )
+    print(data_id, res['MessageId'], data[:8])
