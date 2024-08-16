c17c8a74f5b0e9cb70147b6b0d4f181e78dcf54f
sunghj1118 <sunghj1118@gmail.com>
2024-08-17 08:11:13 +0900

    feat: add .env call for gpt token

--- gpt-timeframe/timeframe.py
+++ gpt-timeframe/timeframe.py
@@ -1,16 +1,17 @@
 import openai
+from dotenv import load_dotenv
 import os
 import pandas as pd
 
-# load_dotenv()
-api_key = ""
+# .env 파일으로부터 API 키를 가져온다
+load_dotenv()
+api_key = os.getenv('GPT_TOKEN')
 
-# Initialize the OpenAI API
+# OpenAI API를 사용하기 위해 API 키를 설정하고 클라이언트를 생성한다
 openai.api_key = api_key
-
 client = openai()
 
-# Make the API call
+# API 호출을 통해 대화를 생성한다
 response = client.chat.completions.create(
   model="gpt-4o-mini",
   messages=[
@@ -18,17 +19,5 @@ response = client.chat.completions.create(
   ]
 )
 
-# Print the response
+# 답변에 해당하는 내용을 출력한다
 print(response.choices[0].message.content)
-
-from openai import OpenAI
-client = OpenAI()
-
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "Who is Heungindaeyo?"},
-  ]
-)
-
-print(completion.choices[0].message)
