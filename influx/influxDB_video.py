8201453fb26bc70bb9bbb152e50bd3a1fdf2e137
sunghj1118 <sunghj1118@gmail.com>
2024-08-20 01:24:49 +0900

    feat: tidy up and change order

--- influx/influxDB_video.py
+++ influx/influxDB_video.py
@@ -29,13 +29,13 @@ bucket = "HighLighter"
 # InfluxDB client 인스턴스를 생성하고, 쿼리 API를 사용하기 위해 인스턴스를 생성한다
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 query_api = client.query_api()
-start_time = "-36h"
+start_time = "-2h"
 
 # "kafka_data"으로부터 "value" 필드만 가져오는 Flux 쿼리를 작성한다
 query = f'''
 from(bucket: "{bucket}")
   |> range(start: {start_time})
-  |> filter(fn: (r) => r["_measurement"] == "kafka_data_1")
+  |> filter(fn: (r) => r["_measurement"] == "video_binary_2")
   |> filter(fn: (r) => r["_field"] == "value")
   |> keep(columns: ["_time", "_value"])
 '''
@@ -56,10 +56,10 @@ def decode_byte_to_ts(df):
   """
   바이트를 .ts로 디코딩하여 파일로 저장한다.
   """
-  video_file_path = "BEAN.ts"
+  video_file_path = "obama.ts"
   if not df.empty:
       first_entry_time = df['time'].iloc[0]
-      end_entry_time = first_entry_time + timedelta(minutes=2)
+      end_entry_time = first_entry_time + timedelta(minutes=30)
       print(f"First entry time: {first_entry_time}, End time: {end_entry_time}")
       
       # 설정한 시간 범위 내의 데이터만 필터링한다
@@ -74,9 +74,25 @@ def decode_byte_to_ts(df):
               decoded_chunk = base64.b64decode(row['video'].encode())
               video_file.write(decoded_chunk)
       
-      print("Video file created at vid.ts")
+      print(f"Video file created at {video_file_path}")
   else:
       print("No data found in the specified time range.")
+
+
+
+def ts_to_mp4(ts_path,mp4_path):
+  """
+  유튜브 업로드를 위해 from .ts to .mp4 변환
+  """
+  ffmpeg_cmd = f"ffmpeg -i {ts_path} -c copy {mp4_path}"
+  command1 = shlex.split(ffmpeg_cmd)
+  try:
+      response=subprocess.run(command1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  except OSError as e:
+      logging.error(e)
+      return False
+  print("변환 성공")
+  return True
 
 
 def upload_file(file_name, bucket, object_name=None):
@@ -95,40 +111,25 @@ def upload_file(file_name, bucket, object_name=None):
   return True
 
 
-def ts_to_mp4(ts_path,mp4_path):
-  """
-  유튜브 업로드를 위해 from .ts to .mp4 변환
-  """
-  ffmpeg_cmd = f"ffmpeg -i {ts_path} -c copy {mp4_path}"
-  command1 = shlex.split(ffmpeg_cmd)
-  try:
-      response=subprocess.run(command1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  except OSError as e:
-      logging.error(e)
-      return False
-  print("변환 성공")
-  return True
-
-
 
 # 환경 변수 불러오기
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_DEFAULT_REGION = "ap-northeast-2" 
+AWS_DEFAULT_REGION = "ap-northeast-2"
 
 
-mp4_path="BEAN.mp4"
-ts_path= "BEAN.ts"
+mp4_path="obama.mp4"
+ts_path= "obama.ts"
 
 bucket_name="ybigta-highlight"
-object_name="BEAN.mp4"
+object_name="obama.mp4"
 
 
 
 # 실행코드: 바이트를 .ts로 디코딩하고 .ts를 .mp4로 변환하여 s3에 업로드
 decode_byte_to_ts(df)
 ts_to_mp4(ts_path,mp4_path)
-upload_file(mp4_path, bucket_name, object_name)
+# upload_file(mp4_path, bucket_name, object_name)
 
 
 # 클라이언트를 종료
